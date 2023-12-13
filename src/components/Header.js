52864c6d82abc21e503576b59523ce42b5c71b94
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-13 10:13:20 -0800

    add Header file

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Ravin's Store</h1>
+      <p>This is my store. </p>
+    </React.Fragment>
+  );
+}
+
+export default Header;

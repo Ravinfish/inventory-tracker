12a543dc3685926857881d63c765ea8349f53b77
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-14 15:12:42 -0800

    wip

--- src/components/StockItem.js
+++ src/components/StockItem.js
@@ -2,5 +2,23 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function StockItems({ name, price, imgSrc, pieces, quantity, id, addToBag }) {
-  const []
+  const [quantityRemaining, setQuantity] = useState(quantity);
+
+  const handleBagClick = () => {
+    if (quantityRemaining > 0) {
+      setQuantity(s => s - 1);
+      addToBag(name, price, id);
+    } else {
+    }
+  };
+
+  const handleUpdateClick = () => {
+    handleUpdateClick(id);
+  };
+
+  return (
+    <div id="stockItem">
+      
+    </div>
+  )
 }

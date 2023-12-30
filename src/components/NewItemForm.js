c56dab36b10c4bbaf69c5109bba97aad0b69ba27
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-29 17:16:08 -0800

    will now display new inventory items

--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -1,89 +1,3 @@
-// import React from "react";
-// import PropTypes from 'prop-types';
-// import { v4 } from 'uuid';
-// import { render } from "@testing-library/react";
-
-// class NewItemDetail extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     const selectedItem = this.props.itemsInStock.find(item => item.id === this.props.selectedItemId);
-
-//     this.state = {
-//       name: selectedItem ? selectedItem.name : "",
-//       price: selectedItem ? selectedItem.price : "",
-//       pieces: selectedItem ? selectedItem.pieces : "",
-//       quantity: selectedItem ? selectedItem.quantity : "",
-//       // imgSrc: selectedItem ? selectedItem.imgSrc : "",
-//     };
-//   }
-
-//   handleNewItemSubmission = (e) => {
-//     e.preventDefault();
-
-//     const itemId = this.props.selectedItemId || v4();
-//     const existingItem = this.props.itemsInStock.find(item => item.id === this.props.selectedId);
-
-//     const updatedItem = {
-//       id: itemId,
-//       name: this.state.name !== "" ? this.state.name : (existingItem ? exsistingItem.name : ""),
-//       price: this.state.price !== "" ? this.state.price : (existingItem ? exsistingItem.price : ""),
-//       pieces: this.state.pieces !== "" ? this.state.pieces : (existingItem ? exsistingItem.pieces : ""),
-//       quantity: this.state.quantity !== "" ? this.state.quantity : (existingItem ? exsistingItem.quantity : ""),
-//       // imgSrc: this.state.imgSrc !== "" ? this.state.imgSrc : this.selectedItem.imgSrc,
-//     };
-
-//     this.props.onNewItemCreation(updatedItem);
-//   };
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <div className="input-form">
-//           <h2>Add New Item</h2>
-//           <form onSubmit={this.handleNewItemSubmission}>
-//             <label>Name: </label>
-//             <input
-//               type="text"
-//               name="name"
-//               defaultValue={this.state.name} />
-//             <label>Price: </label>
-//             <input
-//               type="text"
-//               name="price"
-//               defaultValue={this.state.price} />
-//             <label>Pieces: </label>
-//             <input
-//               type="text"
-//               name="pieces"
-//               defaultValue={this.state.pieces} />
-//             <label>Quantity: </label>
-//             <input
-//               type="number"
-//               name="quantity"
-//               defaultValue={this.state.quantity} />
-//             {/* <label>Image: </label>
-//             <input
-//               type="text"
-//               name="imgSrc"
-//               placeholder="Image Link" /> */}
-//             <br></br>
-//             <button type="submit">Add to Inventory</button>
-//           </form>
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// NewItemDetail.propTypes = {
-//   onNewItemCreation: PropTypes.func.isRequired,
-//   selectedItemId: PropTypes.string,
-//   itemsInStock: PropTypes.array.isRequired,
-// };
-
-// export default NewItemDetail;
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
@@ -118,10 +32,16 @@ class NewItemDetail extends Component {
       price: this.state.price || existingItem.price || "",
       pieces: this.state.pieces || existingItem.pieces || "",
       quantity: this.state.quantity !== "" ? parseInt(this.state.quantity, 10) : existingItem.quantity || 0,
+
     };
   
     onNewItemCreation(updatedItem);
   };
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    
+  }
 
   render() {
     return (
@@ -134,28 +54,32 @@ class NewItemDetail extends Component {
               type="text"
               name="name"
               placeholder="Item Name"
-              defaultValue={this.state.name}
+              value={this.state.name}
+              onChange={this.handleInputChange}
             />
             <label>Price: </label>
             <input
               type="text"
               name="price"
               placeholder="Price"
-              defaultValue={this.state.price}
+              value={this.state.price}
+              onChange={this.handleInputChange}
             />
             <label>Pieces: </label>
             <input
               type="text"
               name="pieces"
               placeholder="Pieces"
-              defaultValue={this.state.pieces}
+              value={this.state.pieces}
+              onChange={this.handleInputChange}
             />
             <label>Quantity: </label>
             <input
               type="number"
               name="quantity"
               placeholder="1"
-              defaultValue={this.state.quantity}
+              value={this.state.quantity}
+              onChange={this.handleInputChange}
             />
             <br></br>
             <button type="submit">Add to Inventory</button>

5a443ab31e2c52414376c3c3a4570a12de57d66c
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-02 07:31:34 -0800

    first test

---
+++ src/__tests__/reducers/inventory-reducer.test.js
@@ -0,0 +1,24 @@
+import inventoryReducer from './inventoryReducer';
+
+describe('inventoryReducer', () => {
+  it('should handle ADD_TO_INVENTORY action', () => {
+    const initialState = {
+      masterInventoryList: [],
+    };
+
+    const action = {
+      type: 'ADD_TO_INVENTORY',
+      payload: {
+        id: '123',
+        name: 'Test Item',
+        price: '19.99',
+        pieces: '10 pieces',
+        quantity: 5,
+      },
+    };
+
+    const nextState = inventoryReducer(initialState, action);
+
+    expect(nextState.masterInventoryList).toHaveLength(1);
+  })
+})

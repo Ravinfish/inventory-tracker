6a1a89b7bbda39f07631dff524e054b22c1ebc21
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-17 11:57:56 -0800

    passed test to add multiple items

--- src/__tests__/reducers/inventory-reducer.test.js
+++ src/__tests__/reducers/inventory-reducer.test.js
@@ -22,4 +22,36 @@ describe('inventoryReducer', () => {
       masterInventoryList: [itemToAdd],
     });
   });
+
+  it('should handle adding mulitple items to inventory', () => {
+    const initialState = {
+      masterInventoryList: [],
+    };
+
+    const item1 = {
+      name: 'Item 1',
+      price: '$9.99',
+      pieces: '5 pieces',
+      quantity: 10,
+      id: 'item1',
+    };
+
+    const item2 = {
+      name: 'Item 2',
+      price: '14.99',
+      pieces: '8 pieces',
+      quantity: 15,
+      id: 'item2',
+    };
+
+    const action1 = addToInventory(item1);
+    const stateAfterAddingItem1 = inventoryReducer(initialState, action1);
+
+    const action2 = addToInventory(item2);
+    const nextState = inventoryReducer(stateAfterAddingItem1, action2);
+
+    expect(nextState).toEqual({
+      masterInventoryList: [item1, item2],
+    });
+  });
 });

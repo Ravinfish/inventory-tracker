3f62c2e92e59588079bcd617fdf9f3070df88f10
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-17 11:26:47 -0800

    update reducers

--- src/reducers/inventory-reducer.js
+++ src/reducers/inventory-reducer.js
@@ -2,19 +2,17 @@ const initialState = {
   masterInventoryList: [],
 };
 
-const action = {
-  type: 'ADD_TO_INVENTORY',
-  payload: {
-    id: '123',
-    name: 'Test Item',
-    price: '19.99',
-    pieces: '10 pieces',
-    quantity: 5,
-  },
+const inventoryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TO_INVENTORY':
+      return {
+        ...state,
+        masterInventoryList: [...state.masterInventoryList, action.payload],
+      };
+
+      default:
+        return state;
+  }
 };
-
-const nextState = inventoryReducer(initialState, action);
-
-expect(nextState.masterInventoryList).toHaveLength(1);
 
 export default inventoryReducer;
---
+++ src/reducers/inventoryActions.js
@@ -0,0 +1,4 @@
+export const addToInventory = (item) => ({
+  type: 'ADD_TO_INVENTORY',
+  payload: item,
+});
